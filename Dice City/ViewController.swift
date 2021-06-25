54a53ecdc6b6513ecf5106eead9ec2dcb39e3e26
Timur Mone <timur@yurtah.com>
2021-06-25 16:05:29 -0400

    Finished the randomization and the app!

--- Dice City/ViewController.swift
+++ Dice City/ViewController.swift
@@ -13,21 +13,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        //                       0,  1,  2,  3,  4,  5,
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        leftDiceNumber += 1
-       
+        diceImageView1.image = diceArray.randomElement()
+        // even shorter, delete Int.random and replace by randomElement()
         
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+//        leftDiceNumber += 1 -> DELETED
         
-        rightDiceNumber -= 1
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+//        rightDiceNumber -= 1 -> DELETED
+        
+        //        print(Int.random(in: 1...10)) // to show the randomizing with range.
+        // 3 dots specify range from 1, including 10
+        // warning with a white circle -> 
     }
     
 }
+
+// refactor - take our code and make it simpler, not repeating or unnecessary
+// Same array is used in both places, why not create a reference, give it a name instead of using an array. Even though xcode shows us nicely, but as soon as you comment it out, it's quite complex. There is an image literal is from source, what if we can create a variable, which then will refer to that array.
