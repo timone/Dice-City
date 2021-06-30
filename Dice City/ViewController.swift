3b9233e1e4c4e372e00f0e947ca27ad60ce09f02
Timur Mone <timur@yurtah.com>
2021-06-30 11:35:59 -0500

    re-factor

--- Dice City/ViewController.swift
+++ Dice City/ViewController.swift
@@ -13,13 +13,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-
-    
-    //    var leftDiceNumber = 1
-    
-    
-
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
@@ -27,18 +20,18 @@ class ViewController: UIViewController {
         diceImageView1.image = diceArray.randomElement()
         // even shorter, delete Int.random and replace by randomElement()
         
-//        leftDiceNumber += 1 -> DELETED
-        
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        
-//        rightDiceNumber -= 1 -> DELETED
-        
-        //        print(Int.random(in: 1...10)) // to show the randomizing with range.
+                
+        // print(Int.random(in: 1...10)) // to show the randomizing with range.
         // 3 dots specify range from 1, including 10
-        // warning with a white circle -> 
+     
     }
     
 }
 
-// refactor - take our code and make it simpler, not repeating or unnecessary
-// Same array is used in both places, why not create a reference, give it a name instead of using an array. Even though xcode shows us nicely, but as soon as you comment it out, it's quite complex. There is an image literal is from source, what if we can create a variable, which then will refer to that array.
+// refactor - take our code and make it simpler,
+// Same array is used in both places, why not create a reference,
+// give it a name instead of using an array. Even though xcode shows
+// us nicely, but as soon as you comment it out, it's quite complex.
+// There is an image literal is from source, what if we can create a variable,
+// which then will refer to that array.
